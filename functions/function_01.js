0e96a10330b39bda1a5064646d3a117c8b8dae6f
RishuK <krishi8271@gmail.com>
2023-12-16 15:48:41 +0530

    updating the funcion_01.js

--- functions/function_01.js
+++ functions/function_01.js
@@ -4,7 +4,7 @@ function addTwoNumver(num1, num2){
 
 // const result = addTwoNumver(3,4)  // 7 output
 const result = addTwoNumver(3,'4')   // 34 output
-console.log(result)
+// console.log(result)
 
 
 function loginUserMessage(username = "User"){      // way to provide default value to the parameter of a function.
@@ -16,7 +16,19 @@ function loginUserMessage(username = "User"){      // way to provide default val
 
 }
 
-console.log(loginUserMessage("Rishu"))  // Rishu just Logged in
+// console.log(loginUserMessage("Rishu"))  // Rishu just Logged in
 
-console.log(loginUserMessage());   // undefined just logged in  
+// console.log(loginUserMessage());   // undefined just logged in  
+
+function calculateCartPrice(num1){
+    return num1;
+}
+console.log(calculateCartPrice(2000));   // output is 2000
+
+function calculateCartPrice(...num1){
+    return num1;
+}
+console.log(calculateCartPrice(2000));   // output is [2000] here the output is in the form of array and '...' is known here as rest operator.
+// similarly we can pass objects, array, as well as numbers in the parameter of a function and then apply buisness logic on it.
+
 
